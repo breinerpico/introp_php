b72144011a65ee6f86ab4d5b2681c4a7ef133589
sistemas <breinerviviescas@colegioguanenta.edu.co>
2025-04-29 09:50:59 -0500

    se desarrollo

---
+++ index.php
@@ -0,0 +1,25 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="widht=device-widht, initial-scale=1.0">
+    <title>introducir a php</title>
+</head>
+<body>
+    <h1>pagina principal del citio</h1>
+    <h2>Breiner Andrey Viviescas Pico</h2>
+    <!--Formulario de inicio de sesion-->
+    <form action="modelo/loguear.php" methop = "post" >
+        <h2>iniciar sesion</h2>
+        <label for="">E-mail:</label>
+        <input type="text" name="usuario" id=""required>
+        <br><br>
+        <label for="">password:</label>
+        <input type="text" name="usuario" id=""required>
+        <br><br>
+        <button type="submit">Ingresar</button>
+    </form>
+</body>
+</html>
+
+
--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,3 +1,35 @@
 <?php
+    //  script para crear una conexion con la BD
 
+    //parametros requeridos para la conexion la BD
+
+    // parametros BD local 
+    /*define('USER','root'); //crea la constante USER con valor 'root'
+    define('PW','');
+    define('HOST', 'localhost');
+    define('BD', 'Empresa');*/
+
+    // parametros BD remota (infinityfree)
+    define('USER','if0_38542088'); //crea la constante USER con valor 'root'
+    define('PW','breinerpico03');
+    define('HOST', 'sql100.infinityfree.com');
+    define('BD', 'if0_38542088_empresa');
+
+    // conexion con la BD
+
+    $conexion = mysqli_connect(HOST,USER,PW,BD);
+
+    // establecer conjunto de caracteres para el hosting 
+    mysqli_set_charset($conexion, "utf8mb4");
+
+    // verificar la conexion con la BD
+    if(!$conexion)
+    {
+         die("la conexion con la BD fallo: " + mysqli_error($conexion));
+         exit();
+    }
+    else
+    {
+        die("conexion a la BD exitosa!");
+    }
 ?>
